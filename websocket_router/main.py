14b6cc1e9407e972a5cf2ecd599f0e23c877f628
laysa siqueira <laysaasiqueiraa2@gmail.com>
2025-06-10 14:35:49 +0100

    teste

--- websocket_router/main.py
+++ websocket_router/main.py
@@ -1,34 +1,25 @@
 import asyncio
 import websockets
 import json
-#from router import route_message
+from router import route_message
 from notificador import connected_clients
 
+# FUNÇÃO OBRIGATÓRIA
+async def handler(websocket, path):
+    print("Novo cliente:", websocket.remote_address)
+    connected_clients.add(websocket)
+    try:
+        async for message in websocket:
+            try:
+                data = json.loads(message)
+                response = await route_message(data)
+            except Exception as e:
+                response = {"error": str(e)}
+            await websocket.send(json.dumps(response))
+    finally:
+        connected_clients.remove(websocket)
 
-#connected_clients = set()
-
-#async def handler(websocket):
-#    print("Novo cliente:", websocket.remote_address)
-    # connected_clients.add(websocket)
-    # try:
-    #     async for message in websocket:
-    #         try:
-    #             data = json.loads(message)
-    #             response = await route_message(data)
-    #         except Exception as e:
-    #             response = {"error": str(e)}
-    #         await websocket.send(json.dumps(response))
-    # finally:
-    #     connected_clients.remove(websocket)
-
-connected_clients = set()
-
-async def notificar_todos(payload):
-    if connected_clients:
-        mensagem = json.dumps({"evento": "novo_usuario", "dados": payload})
-        await asyncio.gather(*(cliente.send(mensagem) for cliente in connected_clients))
-
-
+# FUNÇÃO PRINCIPAL
 async def main():
     async with websockets.serve(handler, "0.0.0.0", 8765):
         print("Servidor WebSocket rodando na porta 8765...")
